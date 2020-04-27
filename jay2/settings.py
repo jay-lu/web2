aed9d5517774f20a9d47fe72842e8a0fa5068ee4
dannyridel <dlc0424@outlook.com>
2020-04-26 21:59:07 -0700

    Profile name

--- jay2/settings.py
+++ jay2/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-if not os.path.isdir(STATIC_ROOT):
-    os.makedirs(STATIC_ROOT, mode=0o755)
-#STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+# if not os.path.isdir(STATIC_ROOT):
+#     os.makedirs(STATIC_ROOT, mode=0o755)
+STATIC_ROOT  =   PROJECT_ROOT #  os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
