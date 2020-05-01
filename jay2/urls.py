58dd4d22fc6115dc7bfbba8e249e5672f7cfea3c
jlu <lujay111@hotmail.com>
2020-04-30 18:36:29 -0700

    photo and video

--- jay2/urls.py
+++ jay2/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import home_view
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('', home_view.home, name='home'),
 ]
